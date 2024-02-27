17d49a6d8e447fd0b7da23be13ef2b5f59780207
Hanzzz123 <aanzs496@gmail.com>
2024-02-27 22:30:31 +0700

    fixed API postman bug

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
 import express from "express";
-
+const app = express()
 import userRoutes from "./routes/users.js"
 import postRoutes from "./routes/posts.js"
 import commentRoutes from "./routes/comments.js"
 import likeRoutes from "./routes/likes.js"
 import authRoutes from "./routes/auth.js"
-import cookieParser from "cookie-parser";
-import cors from "cors";
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 //middlewares
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(cookieParser)
+
 
 app.use("/server/auth",authRoutes)
 app.use("/server/users",userRoutes)
@@ -22,6 +22,6 @@ app.use("/server/likes",likeRoutes)
 app.use("/server/comments",commentRoutes)
 
 
-app.listen(8800,()=>{
+app.listen(3000,()=>{
     console.log("API Working!")
 })
