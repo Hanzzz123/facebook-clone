0e737e47ae99ca2b0da48b987991b250cbceeaa9
Hanzzz123 <aanzs496@gmail.com>
2024-02-27 15:25:27 +0700

    created db table files in route folder

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,11 @@
 import express from "express";
+import {login,register,logout} from "../controllers/auth.js";
 
 const router = express.Router()
 
 router.get("/test",(req,res)=>{
     res.send("it works!")
 })
+
 export default router
 
